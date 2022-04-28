06a3dc218f98996fbe17b75a8e6ac6b50b56fc13
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-04-28 12:22:39 +0100

    Added hero, menu and About components

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import images from '../../constants/images';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <section className='app__header app__wrapper section__padding' id='home'>
+    <article className="app__wrapper_info">
+      <SubHeading title='Chase the new flavour' />
+      <h1 className="app__header-h1">The key to fine dinning.</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint similique, asperiores numquam nihil saepe facere deserunt labore quod deleniti sapiente cum harum earum voluptatem ipsam magni porro assumenda doloremque molestias.
+      </p>
+      <button type='button' className="custom__button">Explore Menu</button>
+    </article>
+    <figure className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome banner image" />
+    </figure>
+  </section>
 );
 
 export default Header;
