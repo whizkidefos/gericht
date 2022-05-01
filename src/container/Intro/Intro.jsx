863aa0e9bcad1a264eee50b684c720c552a07ec5
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-05-01 16:58:24 +0100

    Added Gallery and Footer components

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
+import { images, meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef(false);
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    if(playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  }
+
+  return (
+    <section className='app__video' id='intro'>
+      <video
+        src={meal}
+        ref={vidRef}
+        type='video/mp4'
+        loop
+        muted
+        controls={false}
+      />
+      <div className="app__video-overlay flex__center">
+        <div className="app__video-overlay_circle flex__center" onClick={handleVideo}>
+          {playVideo
+            ? <BsPauseFill color='#fff' fontSize='20px' />
+            : <BsFillPlayFill color='#fff' fontSize='20px' />
+          }
+        </div>
+      </div>
+    </section>
+    )
+    }
 
 export default Intro;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { data, images } from '../../constants';
+
 import './Laurels.css';
 
-const Laurels = () => (
-  <div>
-    Laurels
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <h4 className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</h4>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
   </div>
+);
+
+const Laurels = () => (
+  <section className='app__bg app__wrapper section__padding'>
+
+    <article className="app__wrapper_info">
+      <SubHeading title='Awards & recognition' />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
+
+    </article>
+
+    <figure className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
+    </figure>
+
+  </section>
 );
 
 export default Laurels;
